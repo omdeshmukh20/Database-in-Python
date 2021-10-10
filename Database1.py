ee028ba72fcf95cf5a4542e0dd0bad510198585f
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-10-10 09:41:11 +0530

    Display All Records Of Employee table

---
+++ Database1.py
@@ -0,0 +1,41 @@
+#Function Name    :  Display All Rows From Employee Table
+#Function Date    :  10 Oct 2021
+#Function Author  :  Om Deshmukh
+#Input            :  ----
+#Output           :  It Display All Records Of Employee table
+
+
+# Here I Use MySQL Workbench / MySQL 8.0 Command Line Client
+
+import MySQLdb
+from MySQLdb.cursors import Cursor
+
+def Connection():
+    
+    Connection = MySQLdb.connect(host = "localhost", database = "pythondb", user = "root", password = "-----")
+
+    cobj = Connection.cursor()
+    cobj.execute("select * from emptab")
+
+    '''row = cobj.fetchone()
+                                  OR
+    while row != None:
+        print(row)
+        row = cobj.fetchone()'''
+
+    rows = cobj.fetchall()
+    
+    print("Total Number of Rows are : ", cobj.rowcount)
+
+    for row in rows:
+        print(row)
+
+    cobj.close()
+    Connection.close()
+
+def main():
+    
+    Connection()
+
+if __name__ == "__main__":
+    main()
